20834779d9bf7f02cf0dff1792aeac6335e276c0
Stephanie Wiggins <stephawiggins@gmail.com>
2021-12-02 10:05:39 -0600

    refactors day 2

--- day2/day2.rb
+++ day2/day2.rb
@@ -2,7 +2,6 @@ require 'pry'
 
 # movements = File.readlines('subset.txt').map { |n| n.split(" ") }.to_a
 movements = File.readlines('input.txt').map { |n| n.split(" ") }.to_a
-# print movements
 
 @forward = 0
 @depth = 0
@@ -11,13 +10,14 @@ movements = File.readlines('input.txt').map { |n| n.split(" ") }.to_a
 # Part 1
 def navigate(movements)
   movements.each do |movement|
+    spaces = movement[1].to_i
     case movement[0]
     when "forward"
-      @forward += movement[1].to_i
+      @forward += spaces
     when "down"
-      @depth += movement[1].to_i
+      @depth += spaces
     when "up"
-      @depth -= movement[1].to_i
+      @depth -= spaces
     else
       "Error"
     end
@@ -26,21 +26,18 @@ end
 
 # navigate(movements)
 
-# puts @forward
-# puts @depth
-
-
 # Part 2
 def navigate_with_aim(movements)
   movements.each do |movement|
+    spaces = movement[1].to_i
     case movement[0]
     when "forward"
-      @forward += movement[1].to_i
-      @depth += movement[1].to_i * @aim
+      @forward += spaces
+      @depth += spaces * @aim
     when "down"
-      @aim += movement[1].to_i
+      @aim += spaces
     when "up"
-      @aim -= movement[1].to_i
+      @aim -= spaces
     else
       "Error"
     end
